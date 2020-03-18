1576b2f11ed7dda33a0283a7b0bfcaa23aacb03d
Džemil Džigal <ddzigal1@etf.unsa.ba>
2020-03-18 20:00:41 +0100

    Addition of Services Interfaces

--- EventService/src/main/java/com/lambda/EventService/Services/IEventService.java
+++ EventService/src/main/java/com/lambda/EventService/Services/IEventService.java
@@ -1,4 +1,8 @@
 package com.lambda.EventService.Services;
 
+import com.lambda.EventService.models.Event;
+
 public interface IEventService {
+    void createEvent(Event info);
+    Event findById(long id);
 }
