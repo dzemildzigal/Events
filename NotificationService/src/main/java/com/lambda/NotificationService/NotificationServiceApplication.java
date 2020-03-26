925dfe05a89d1682b993dc0e2cddbc0a236eada8
mhodo1 <midhathodo1@gmail.com>
2020-03-26 20:28:23 +0100

    [NotificationService] swagger added

--- NotificationService/src/main/java/com/lambda/NotificationService/NotificationServiceApplication.java
+++ NotificationService/src/main/java/com/lambda/NotificationService/NotificationServiceApplication.java
@@ -9,8 +9,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.math.BigDecimal;
+
 
 @SpringBootApplication
 public class NotificationServiceApplication {
--- UserTicketService/src/main/java/com/lambda/UserTicketService/repository/ICCPaymentRepository.java
+++ UserTicketService/src/main/java/com/lambda/UserTicketService/repository/ICCPaymentRepository.java
@@ -3,6 +3,8 @@ package com.lambda.UserTicketService.repository;
 import com.lambda.UserTicketService.model.CCPayment;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ICCPaymentRepository extends CrudRepository<CCPayment, Long> {
+import java.util.List;
 
+public interface ICCPaymentRepository extends CrudRepository<CCPayment, Long> {
+    List<CCPayment> findByUserTicketUserId (long userId);
 }
