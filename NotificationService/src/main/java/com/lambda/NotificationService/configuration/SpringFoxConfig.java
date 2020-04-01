d4c79a81bf66874f681fc34c4f088d68157bd56b
lejlab <lbegic1@etf.unsa.ba>
2020-04-01 17:24:53 +0200

    Merge branch 'master' of C:\Users\User\Documents\Events with conflicts.

--- NotificationService/src/main/java/com/lambda/NotificationService/configuration/SpringFoxConfig.java
+++ NotificationService/src/main/java/com/lambda/NotificationService/configuration/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package com.lambda.NotificationService.configuration;
+package com.lambda.NotificationService.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,12 +20,23 @@ import java.util.ArrayList;
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
-
+        ParameterBuilder aParameterBuilder = new ParameterBuilder();
+        aParameterBuilder.name("Authorization")                 // name of header
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")               // type - header
+                .required(false)                // for compulsory
+                .build();
+        java.util.List<Parameter> aParameters = new ArrayList<>();
+        aParameters.add(aParameterBuilder.build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(aParameters);
     }
 
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
 }
