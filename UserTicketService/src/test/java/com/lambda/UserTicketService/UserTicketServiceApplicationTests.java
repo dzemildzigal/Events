eb347acf890ae2873cfe6ca6978088d295bef2ba
lejlab <lbegic1@etf.unsa.ba>
2020-04-02 09:58:10 +0200

    UserTicketService ExceptionHandling

--- UserTicketService/src/test/java/com/lambda/UserTicketService/UserTicketServiceApplicationTests.java
+++ UserTicketService/src/test/java/com/lambda/UserTicketService/UserTicketServiceApplicationTests.java
@@ -2,6 +2,10 @@ package com.lambda.UserTicketService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class UserTicketServiceApplicationTests {
@@ -9,5 +13,15 @@ class UserTicketServiceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	/*
+	@Test
+	void createNotification() throws Exception {
 
+		mockMvc.perform(MockMvcRequestBuilders
+				.post("/notifications/newnotification")
+				.content(asJsonString(new UserNotification(null, null,null,false)))
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().is5xxServerError());
+	}*/
 }
