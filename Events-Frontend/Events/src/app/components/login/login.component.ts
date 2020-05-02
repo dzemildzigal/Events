f88644c011c3d150e80e02a74387ebf5194250f2
mhodo1 <midhathodo1@gmail.com>
2020-05-02 16:57:49 +0200

    [Events Frontent] initial project, and initial login/register forms component

---
+++ Events-Frontend/Events/src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  // form: FormGroup = new FormGroup({
+  //   username: new FormControl(''),
+  //   password: new FormControl(''),
+  // });
+
+  submit(): void {
+    
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
