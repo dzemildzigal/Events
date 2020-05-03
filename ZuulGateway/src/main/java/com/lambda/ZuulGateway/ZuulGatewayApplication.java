57175229225af97f7ee9822cae476b80c74172bc
Džemil Džigal <ddzigal1@etf.unsa.ba>
2020-05-03 12:03:10 +0200

    [ZuulGateway] Initial Zuul Gateway project setup

---
+++ ZuulGateway/src/main/java/com/lambda/ZuulGateway/ZuulGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.lambda.ZuulGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+		System.out.println("Test");
+	}
+
+}
