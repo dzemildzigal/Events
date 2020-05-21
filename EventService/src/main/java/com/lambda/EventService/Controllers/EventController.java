168a78cf25235c4adcd1704c99e6e56af69a931b
lejlab <lbegic1@etf.unsa.ba>
2020-05-21 18:31:44 +0200

    CORS changes

--- EventService/src/main/java/com/lambda/EventService/Controllers/EventController.java
+++ EventService/src/main/java/com/lambda/EventService/Controllers/EventController.java
@@ -26,7 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("event")
 public class EventController {
